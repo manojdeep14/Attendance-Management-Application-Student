ddbf382195eb63cc4a430fdba72940f67aaadcf2
muthusk07 <msmuthu.sk@gmail.com>
2020-03-15 15:27:12 +0530

    Initial commit

---
+++ app/src/androidTest/java/com/developerdesk9/attendanceregister/loginsucces.java
@@ -0,0 +1,116 @@
+package com.developerdesk9.attendanceregister;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class loginsucces {
+
+    @Rule
+    public ActivityTestRule<splashscreen> mActivityTestRule = new ActivityTestRule<>(splashscreen.class);
+
+    @Test
+    public void loginsucces() {
+        ViewInteraction appCompatEditText = onView(
+                allOf(withId(R.id.username_et),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        0),
+                                1),
+                        isDisplayed()));
+        appCompatEditText.perform(replaceText("4"), closeSoftKeyboard());
+
+        ViewInteraction textInputEditText = onView(
+                allOf(childAtPosition(
+                        childAtPosition(
+                                withId(R.id.password_et),
+                                0),
+                        0),
+                        isDisplayed()));
+        textInputEditText.perform(replaceText("123456"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText2 = onView(
+                allOf(withId(R.id.username_et), withText("4"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        0),
+                                1),
+                        isDisplayed()));
+        appCompatEditText2.perform(replaceText("41"));
+
+        ViewInteraction appCompatEditText3 = onView(
+                allOf(withId(R.id.username_et), withText("41"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        0),
+                                1),
+                        isDisplayed()));
+        appCompatEditText3.perform(closeSoftKeyboard());
+
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(R.id.loginButton), withText("Login"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        0),
+                                3),
+                        isDisplayed()));
+        appCompatButton.perform(click());
+
+        ViewInteraction linearLayout = onView(
+                allOf(childAtPosition(
+                        IsInstanceOf.<View>instanceOf(android.widget.FrameLayout.class),
+                        0),
+                        isDisplayed()));
+        linearLayout.check(matches(isDisplayed()));
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
---
+++ app/src/main/java/com/developerdesk9/attendanceregister/lessthan75.java
@@ -0,0 +1,32 @@
+package com.developerdesk9.attendanceregister;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class lessthan75 extends AppCompatActivity {
+    ListView lv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lessthan75);
+        lv=findViewById(R.id.lessthan75);
+        //android.R.layout.simple_list_item_1
+        ArrayList<String> list=getIntent().getExtras().getStringArrayList("less");
+        ArrayAdapter<String> ada=new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                list);
+        lv.setAdapter(ada);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+        });
+    }
+}
